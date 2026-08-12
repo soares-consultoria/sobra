c33356f897384b478d799f0b6d1450bbcfd53a28
soares-consultoria <elieziobr@gmail.com>
2026-08-12 16:25:41 -0300

    Banco: ignorar transferencias entre as proprias contas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sobra-v8';
+const CACHE = 'sobra-v9';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
